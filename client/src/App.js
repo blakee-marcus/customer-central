6046c336b26723a9d2b29d7c1b5e23fe2a450915
bm <marcusb733@gmail.com>
2023-02-24 19:03:43 -0800

    Heroku

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ const Signup = lazy(() => import('./pages/Signup'));
 const Customer = lazy(() => import('./pages/Customer'));
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
